fefc1a728e7f72c1f13f9058205914f2c3ff73e6
MOLEK <MOLEK@DESKTOP-6GSSJ7D>
2019-05-29 21:08:51 +0300

    DocType all

--- src/Entity/PlaceOfInterest.java
+++ src/Entity/PlaceOfInterest.java
@@ -1,10 +1,10 @@
 package Entity;
 /**
- * @author      Gilad Molek 
- * @version     1               
+ * @author MOLEK 
+ * @version 1            
  * @since       1        
  */
-/*
+/**
  * complete
  * */
 import java.util.ArrayList;
@@ -16,79 +16,152 @@ public class PlaceOfInterest {
 	private String _classifcation=null;
 	private String _shortExplanation=null;
 	private boolean _accessibleToSpecialNeeds;
-	
+	/**
+	 * Description of PlaceOfInterest constructor
+	 * @param _name String
+	 * @param _classifcation String
+	 * @param _shortExplanation String
+	 * @param _accessibleToSpecialNeeds Boolean
+	 * 
+	 * Initializing all the parameters also the two arrayLists
+	 * 		ArrayList<Map> _mapsArrayContainsThisPlaceOfInterest
+	 * 		ArrayList<Tour> _tourArrayContainsThisPlaceOfInterest
+	 * */
 	public PlaceOfInterest(String _name,String _classifcation,String _shortExplanation,boolean _accessibleToSpecialNeeds)
 	{
-		/*creates new empty _mapsArrayContainsThisPlaceOfInterest
-		 * creates new empty _tourArrayContainsThisPlaceOfInterest
-		 * constructor gets string name string classification string shortExplantion,boolean accessibleToSpecialNeeds*/
 		this._mapsArrayContainsThisPlaceOfInterest=new ArrayList<Map>();
 		this._tourArrayContainsThisPlaceOfInterest=new ArrayList<Tour>();
 		this._name=_name;
 		this._classifcation=_classifcation;
 		this._accessibleToSpecialNeeds=_accessibleToSpecialNeeds;
 	}
-	
+	/**
+	 * Description of getName method
+	 * @return _name 	type String
+	 * 
+	 * returns String Name Of place of interest
+	 * */
 	public String getName()
 	{
 		return this._name;
 	}
-	
+	/**
+	 * Description of getClassifcation method
+	 * @return _classifcation 	type String
+	 * 
+	 * returns String Description Of place of interest
+	 * */
 	public String getClassifcation()
 	{
 		return this._classifcation;
 	}
-	
+	/**
+	 * Description of getShortExplanation method
+	 * @return _shortExplanation 	type String
+	 * 
+	 * returns String short Explanation Of place of interest
+	 * */
 	public String getShortExplanation()
 	{
 		return this._shortExplanation;
 	}
-	
+	/**
+	 * Description of getShortExplanation method
+	 * @return _accessibleToSpecialNeeds 	type Boolean
+	 * 
+	 * returns Boolean if place of interest is accessible To Special Needs
+	 * */
 	public boolean getAccessibleToSpecialNeeds()
 	{
 		return this._accessibleToSpecialNeeds;
 	}
-	
+	/**
+	 * Description of getMapsArrayContainsThisPlaceOfInterest method
+	 * @return _mapsArrayContainsThisPlaceOfInterest 	type ArrayList<Map>
+	 * 
+	 * returns ArrayList of the maps that contains this place of interest
+	 * */
 	public ArrayList<Map> getMapsArrayContainsThisPlaceOfInterest()
 	{
 		return this._mapsArrayContainsThisPlaceOfInterest;
 	}
-	
+	/**
+	 * Description of getTourArrayContainsThisPlaceOfInterest method
+	 * @return _tourArrayContainsThisPlaceOfInterest 	type ArrayList<Tour>
+	 * 
+	 * returns ArrayList of the Tours that contains this place of interest
+	 * */
 	public ArrayList<Tour> getTourArrayContainsThisPlaceOfInterest()
 	{
 		return this._tourArrayContainsThisPlaceOfInterest;
 	}
-	
+	/**
+	 * Description of setName method
+	 * @param _name 	type String
+	 * 
+	 * set changes in the place of interest name
+	 * */
 	public void setName(String _name)
 	{
 		this._name=_name;
 	}
-	
+	/**
+	 * Description of setClassifcation method
+	 * @param _classifcation 	type String
+	 * 
+	 * set changes in the place of interest Classification
+	 * */
 	public void setClassifcation(String _classifcation)
 	{
 		this._classifcation=_classifcation;
 	}
-	
+	/**
+	 * Description of setShortExplanation method
+	 * @param _shortExplantion 	type String
+	 * 
+	 * set changes in the place of interest Short explanation
+	 * */
 	public void setShortExplanation(String _shortExplantion)
 	{
 		this._shortExplanation=_shortExplanation;
 	}
-	
+	/**
+	 * Description of setAccesibleToSpecialNeeds method
+	 * @param _accesibleToSpecialNeeds 	type boolean
+	 * 
+	 * set true/false if the place of interest is accessible to special needs
+	 * */
 	public void setAccesibleToSpecialNeeds(boolean _accesibleToSpecialNeeds)
 	{
 		this._accessibleToSpecialNeeds=_accessibleToSpecialNeeds;
 	}
-	
+	/**
+	 * Description of addToMapsArrayContainsThisPlaceOfInterest method
+	 * @param map 	type Map
+	 * 
+	 * adds map to the map arrayList of all maps that contains this place of interest
+	 * */
 	public void addToMapsArrayContainsThisPlaceOfInterest(Map map)
 	{
 		this._mapsArrayContainsThisPlaceOfInterest.add(map);
 	}
-	
+	/**
+	 * Description of addToTourArrayContainsThisPlaceOfInterest method
+	 * @param tour 	type Tour
+	 * 
+	 * adds tour to the tour arrayList of all tours that contains this place of interest
+	 * */
 	public void addToTourArrayContainsThisPlaceOfInterest(Tour tour)
 	{
 		this._tourArrayContainsThisPlaceOfInterest.add(tour);
 	}
-	
+	/**
+	 * Description of getMapAtIndexInMapsArrayContainsThisPlaceOfInterest method
+	 * @param index 	type int
+	 * 
+	 * @return Map 
+	 * returns map at index in MapsArrayContainsThisPlaceOfInterest arraylist
+	 * */
 	public Map getMapAtIndexInMapsArrayContainsThisPlaceOfInterest(int index)
 	{
 		return this._mapsArrayContainsThisPlaceOfInterest.get(index);
--- src/Entity/Version.java
+++ src/Entity/Version.java
@@ -12,7 +12,11 @@ public class Version {
 	private City _city=null;
 	private double _versionNumber=0.0;
 	private String _description=null;
-	
+	/**Description of Version constructor
+	 * @param _city							City Object type
+	 * @param _versionNumber				double type
+	 * @param _description					String type
+	 * */
 	public Version(City _city,double _versionNumber,String _description)
 	{
 		/*constructor get City Object, double versionNumber' String description */
@@ -20,31 +24,52 @@ public class Version {
 		this._versionNumber=_versionNumber;
 		this._description=_description;
 	}
-	public City getCityName()
+	/**Description of getCity method
+	 *@return City                          the City of the version as City Object type
+	 *
+	 *returns the city
+	 * */
+	public City getCity()
 	{
 		return this._city;
 	}
-	
-	public void setCityName(City _city)
+	/**Description of setCity method
+	 *@param   _city                       City type
+	 *
+	 *set city of this version
+	 * */
+	public void setCity(City _city)
 	{
 		this._city=_city;
 	}
-	
+	/**Description of getVersionNumber method
+	 *@return   _versionNumber                       double type
+	 * */
 	public double getVersionNumber() 
 	{
 		return this._versionNumber;
 	}
-	
+	/**Description of setVersionNumber method
+	 *@param   _versionNumber                       double type
+	 *
+	 *set version number of this version
+	 * */
 	public void setVersionNumber(double _versionNumber)
 	{
 		this._versionNumber=_versionNumber;
 	}
-	
+	/**Description of getDescription method
+	 *@return   _description                       String type
+	 * */
 	public String getDescription()
 	{
 		return _description;
 	}
-	
+	/**Description of setDescription method
+	 *@param   _description                       String type
+	 *
+	 *set description of this version
+	 * */
 	public void setDescription(String _description)
 	{
 		this._description=_description;
